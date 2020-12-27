2cd850802a31aa841ab8ef6004e55753fc4b7b45
unknown <adis.mehuric@hotmail.com>
2020-12-27 19:05:10 +0100

    add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,8 +16,8 @@ const forecast = (latitude, longitude, callback) => {
           body.current.temperature +
           ' degrees out. It feels like ' +
           body.current.feelslike +
-          ' degrees out. Humidity is: ' +
-          body.current.humidity
+          ' degrees out. The humidity is: ' +
+          body.current.humidity + "%."
       );
     }
   });
